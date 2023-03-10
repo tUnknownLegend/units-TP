e8de3b140cf18ef9d0d31ca3506d25734ba3a0c9
Aint <pinevichv@gmail.com>
2023-03-10 19:15:15 +0300

    splited SortButton component test

--- src/components/SortButton/SortButton.test.tsx
+++ src/components/SortButton/SortButton.test.tsx
@@ -7,7 +7,19 @@ import { SortButton } from './SortButton';
 afterEach(jest.clearAllMocks);
 
 describe('Button render', () => {
-    it('should contain sort text', () => {
+    const noop = () => {};
+
+    it('SortButton snapshot and button text', () => {
+        const renderedButton = render(
+            <SortButton currentSort="по умолчанию" onSortButtonClick={noop} />
+        );
+        expect(renderedButton.getByRole('button')).toHaveTextContent(
+            'Сортировать по умолчанию'
+        );
+        expect(renderedButton.asFragment()).toMatchSnapshot();
+    });
+
+    it('checks sort button click and ', () => {
         const onSortButtonClick = jest.fn();
         const renderedButton = render(
             <SortButton
@@ -15,7 +27,6 @@ describe('Button render', () => {
                 onSortButtonClick={onSortButtonClick}
             />
         );
-
         expect(renderedButton.getByRole('button')).toHaveTextContent(
             'Сортировать по умолчанию'
         );
@@ -23,7 +34,5 @@ describe('Button render', () => {
         expect(onSortButtonClick).toHaveBeenCalledTimes(0);
         fireEvent.click(renderedButton.getByRole('button'));
         expect(onSortButtonClick).toHaveBeenCalledTimes(1);
-
-        expect(renderedButton.asFragment()).toMatchSnapshot();
     });
 });
